e9e455dca011caa6904e33204dcf77f2833ea6c2
dusiyong <dusiyong@yy.com>
2021-03-07 23:02:29 +0800

    add

--- app/service/getHtml.js
+++ app/service/getHtml.js
@@ -7,9 +7,12 @@ const HOST = 'http://www.itheima.com/images/teacher/ui/'
 class GetHtml extends Service {
   async gethtmlData(){
     const { ctx, config } = this
-    const {data} = await ctx.curl(`http://web.itheima.com/teacher.html`)
+    // const {data} = await ctx.curl(`http://web.itheima.com/teacher.html`)
+    const {data} = await ctx.curl(`http://share.teachee-dev.com/share/batch/113/7982151405935329280?urlDetail==https%3A%2F%2Fdsy.teachee-dev.com%3A8091%2Fstudent%2Fcenter%2Fstore%2Fbatch-detail%3Fid%3D113`)
     const chunks = new Buffer(JSON.parse(JSON.stringify(data))).toString() // Buffer转JSON
+    console.log('chunks:',chunks)
     const $ =  cheerio.load(chunks) // 解析html
+    console.log('$:',$)
     const imgList = Array.prototype.map.call($('.tea_box4 .maincon .clears .main_pic  > img'),item=>{
       const  itemData= $(item).attr('src').split('/').slice(-1)
       return encodeURI(HOST+itemData[0])
